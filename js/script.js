8617f3a8a30bff107801fb95d8cbfcc05f735fdd
Sergey <sdiomant@gmail.com>
2021-12-07 18:43:42 +0200

    HeaderAboutGalleryFooter

--- js/script.js
+++ js/script.js
@@ -1,20 +1,12 @@
 "use strict"
-
+// Меню бургер
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
-
-iconMenu.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    iconMenu.classList.toggle("_active");
-    menuBody.classList.toggle("_active");
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
 }
 
-const navLink = document.querySelectorAll(".menu__link");
-
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-    iconMenu.classList.remove("_active");
-    menuBody.classList.remove("_active");
-}
